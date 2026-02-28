92e4efd624bae38fbcc71b668b5c611fb5f0eb6b
Jituboh <davidjituboh578@gmail.com>
2026-02-28 21:45:02 +0100

    Add core configuration module for loading environment variables

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,1 +1,15 @@
-# Configuration settings for the application
+# Configuration settings
+
+from dotenv import load_dotenv
+import os 
+
+# Load environment variables from .env file
+load_dotenv()
+
+#Class to hold configuration settings
+class Settings:
+    SUPABASE_URL: str = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
+
+#Instance of class Settings
+settings = Settings()
